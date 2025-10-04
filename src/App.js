ba1ec2ebe200c86de367d80cbe1169469f19330d
Andrew <shitovandrey@list.ru>
2025-10-04 20:02:53 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 function App() {
   return (
     <div className="wrapper clear">
+
+      <div className="shadowOverlay">
+        <div className="drawer">
+          <h2 className="mb-40">Корзина</h2>
+         
+
+        <div className="items">
+           <div className="cartItem mb-20 d-flex align-center">
+            <div className="cartItem-img" style={{backgroundImage:'url("/img/shoes/img-1.jpg")'}}></div>
+            <div className="mr-20">
+            <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
+            <b>12 999 руб.</b>
+          </div>
+          <img className="CartItem-btn" src="/img/btn-removed.svg" alt="remove" />
+        </div>
+        <div className="cartItem mb-20 d-flex align-center">
+            <div className="cartItem-img" style={{backgroundImage:'url("/img/shoes/img-1.jpg")'}}></div>
+            <div className="mr-20">
+            <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
+            <b>12 999 руб.</b>
+          </div>
+          <img className="CartItem-btn" src="/img/btn-removed.svg" alt="remove" />
+        </div>
+        </div>
+        <div className="cartTotalBlock">
+          <ul>
+          <li className="d-flex">
+            <span>Итого:</span>
+            <div></div>
+            <b>21 498руб.</b>
+          </li>
+          <li className="d-flex">
+            <span>Налог 5%:</span>
+            <div></div>
+            <b>1074 руб.</b>
+          </li>
+        </ul>
+        <button>Оформить заказ</button>
+        </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="headerLeft d-flex align-center">
           <img className="logo" src="/img/logo.svg"/>
